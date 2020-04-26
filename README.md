f17df1a24cb3d8c7a1e0bf9376e91c24476f0b70
Victor Ribeiro <jvictorjy@gmail.com>
2020-04-25 22:18:00 -0300

    :sparkles: Challange NodeJS fundamentals finalized

---
+++ README.md
@@ -0,0 +1,4 @@
+## Aluno: Victor Ribeiro
+<h3>
+    Desafio 05: Fundamentos de NodeJS
+</h3>
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+const transactionRepository = new TransactionsRepository();
 
 const transactionRouter = Router();
 
@@ -9,7 +11,10 @@ const transactionRouter = Router();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionRepository.all();
+    const balance = transactionRepository.getBalance();
+
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +22,19 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionService(
+      transactionRepository,
+    );
+
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
